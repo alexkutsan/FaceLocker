d5a1c7d5e0e7f4b762c0b699988327f6b9d0bb6f
Alex Kutsan <akutsan@luxoft.com>
2018-03-09 17:48:00 +0200

    install qtcreator in docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,6 @@
 
 docker run -it --rm --device /dev/video0  \
+	   -e LINES=$(tput lines) -e COLUMNS=$(tput cols) \
            -e LOCAL_USER_ID=`id -u $USER` -e DISPLAY=$DISPLAY \
            --security-opt seccomp=unconfined \
            -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/fun:/home/developer/fun \
--- main.cc
+++ main.cc
@@ -1,80 +1,65 @@
-// The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
-/*
-
-    This example program shows how to find frontal human faces in an image and
-    estimate their pose.  The pose takes the form of 68 landmarks.  These are
-    points on the face such as the corners of the mouth, along the eyebrows, on
-    the eyes, and so forth.  
-    
-
-    This example is essentially just a version of the face_landmark_detection_ex.cpp
-    example modified to use OpenCV's VideoCapture object to read from a camera instead 
-    of files.
-
-
-    Finally, note that the face detector is fastest when compiled with at least
-    SSE2 instructions enabled.  So if you are using a PC with an Intel or AMD
-    chip then you should enable at least SSE2 instructions.  If you are using
-    cmake to compile this program you can enable them by using one of the
-    following commands when you create the build project:
-        cmake path_to_dlib_root/examples -DUSE_SSE2_INSTRUCTIONS=ON
-        cmake path_to_dlib_root/examples -DUSE_SSE4_INSTRUCTIONS=ON
-        cmake path_to_dlib_root/examples -DUSE_AVX_INSTRUCTIONS=ON
-    This will set the appropriate compiler options for GCC, clang, Visual
-    Studio, or the Intel compiler.  If you are using another compiler then you
-    need to consult your compiler's manual to determine how to enable these
-    instructions.  Note that AVX is the fastest but requires a CPU from at least
-    2011.  SSE4 is the next fastest and is supported by most current machines.  
-*/
-
-#include <dlib/opencv.h>
-#include <opencv2/highgui/highgui.hpp>
+#include <dlib/dnn.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
 #include <dlib/image_processing/frontal_face_detector.h>
-#include <dlib/image_processing.h>
+#include "static_models.h"
 
 using namespace dlib;
 
+StaticModels models;
+
+std::vector<matrix<rgb_pixel>> get_faces(const std::string image_path) {
+    auto& detector = models.face_detector();
+    
+    std::vector<matrix<rgb_pixel>> faces;
+    dlib::matrix<rgb_pixel> img;
+    dlib::load_image(img, image_path);
+    auto detected_faces = detector(img);
+    for (auto& face : detected_faces) {
+        auto shape = models.shape_predictor()(img, face);
+        dlib::matrix<rgb_pixel> face_chip;
+        dlib::extract_image_chip(img,
+                                dlib::get_face_chip_details(shape,150,0.25),
+                                face_chip);
+        faces.push_back(std::move(face_chip));
+    }
+    return faces;
+} 
+
+std::vector<matrix<rgb_pixel>> get_faces(const std::vector<std::string>& images) {
+    std::vector<matrix<rgb_pixel>> faces;
+    for(const auto& image_path: images) {
+        auto image_faces = get_faces(image_path);
+        faces.insert(faces.end(), image_faces.begin(),image_faces.end());
+    }
+    return faces;
+}
+
 int main(int argc, char** argv) {
     try {
-
-        // Load face detection and pose estimation models.
-        frontal_face_detector detector = get_frontal_face_detector();
-        shape_predictor pose_model;
-        deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
-
-        for (int i = 1 ; i < argc; ++i) {
-            cv::Mat temp = cv::imread(argv[i], CV_LOAD_IMAGE_COLOR);  
-            cv_image<bgr_pixel> cimg(temp);
-
-            // Detect faces 
-            std::vector<rectangle> faces = detector(cimg);
-            if (faces.size() > 0 ) {
-                std::cout << "Face found : " << argv[i]  << std::endl;
-            } else {
-                std::cout << "Face not found : " << argv[i]  << std::endl;
-            }
-            // for (auto& face: faces) {
-            //     cv::rectangle(temp, cv::Point(face.left(), face.bottom()),
-            //                         cv::Point(face.right(), face.top()),
-            //                         cv::Scalar(255,255,255), 2);
-            // }
-            
-            // cv::imshow("cam", temp);
-            // if (cv::waitKey(30) >= 0) {
-            //     break;
-            // }  
+        if (argc == 1) {
+            throw std::invalid_argument("No arguments provided");
+        }
+        std::vector<std::string> input_data(argv + 1, argv + argc);
+        auto faces = get_faces(input_data);
+        std::vector<dlib::matrix<float,0,1>> face_descriptors = models.network()(faces);
+        for(const auto& face1 : face_descriptors) {
+         for(const auto& face2 : face_descriptors) {
+            std::cout << dlib::length(face1 - face2) << " ";
+         }
+         std::cout << std::endl;
         }
     }
-    catch(serialization_error& e)
-    {
+    catch(dlib::serialization_error& e) {
         using namespace std;
         cout << "You need dlib's default face landmarking model file to run this example." << endl;
         cout << "You can get it from the following URL: " << endl;
         cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
         cout << endl << e.what() << endl;
     }
-    catch(std::exception& e)
-    {
+    catch(std::exception& e) {
         std::cout << e.what() << std::endl;
     }
 }
